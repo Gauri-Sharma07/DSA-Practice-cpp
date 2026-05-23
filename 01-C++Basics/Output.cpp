537d5ed29c338a7b40dd6eaa100838017ed97ee2
Gauri-Sharma07 <sharma.gauriofficial@gmail.com>
2026-05-23 23:24:40 +0530

    feat: complete user input output foundation challenge

---
+++ 01-C++Basics/Output.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "My First C++ Code!";
+   
+   return 0;
+}
--- 01-C++Basics/user_input.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-int main() {
-    int x;
-    std::cout << "Enter a number: ";
-    std::cin >> x;
-    std::cout << "You entered: " << x << std::endl;
-    return 0;
-}
